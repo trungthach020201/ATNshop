2fce574e6fc987248ca561cc1c4376b52ca6b4ac
“trungthach020201” <wendythach489@gmail.com>
2021-11-07 21:29:37 +0700

    ok

--- updateprod.php
+++ updateprod.php
@@ -79,7 +79,7 @@
                                 <div class="form-group">  
                                     <label for="sphinhanh" class="col-sm-2 control-label">Image(*):  </label>
                                             <div class="col-sm-10">
-                                                <input type="file" name="txtImage" id="txtImage" class="form-control" value=""/>
+                                                <input type="file" name="txtImage" id="txtImage" class="form-control" value='<?php echo $pic ?>'/>
                                             </div>
                                         </div>
                                         
