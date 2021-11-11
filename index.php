568e83928082eecf4d5cd9f702b48a383b0b6b04
“trungthach020201” <wendythach489@gmail.com>
2021-11-11 16:26:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -156,7 +156,7 @@ if(isset($_GET['page']))
     elseif($page=="updateprod"){
         include_once("updateprod.php");
     }
-    elseif($page=="shops_management"){
+    elseif($page=="addtocart"){
         include_once("addtocart.php");
     }
     elseif($page=="addshop"){
